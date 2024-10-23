7cced8686c17f58857dce6e76f4f8858ba10854e
Michal Zbranek <michal.zbranek.1@gmail.com>
2024-10-23 10:23:14 +0200

    split into smaller files

--- src/data.ts
+++ src/data.ts
@@ -1,57 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import initialDataState from "./prepareInitialData";
-
-const deleteElement = (state: ReduxInnerType, uuid: string) => {
-  // recursively apply deleteElement on every children
-  if (state.value[`${uuid}`].childrens.length !== 0) {
-    state.value[`${uuid}`].childrens.map((childrenUuid: string) =>
-      deleteElement(state, childrenUuid)
-    );
-  }
-  // delete UUID in parent array
-  const parentUUID = state.value[`${uuid}`].parentUUID;
-  if (parentUUID !== undefined) {
-    state.value[`${parentUUID}`].childrens = state.value[
-      `${parentUUID}`
-    ].childrens.filter((childrenUUID: string) => childrenUUID !== uuid);
-  } else {
-    state.value.root.childrens = state.value.root.childrens.filter(
-      (baseUUID: string) => baseUUID !== uuid
-    );
-  }
-  // delete element
-  delete state.value[uuid];
-};
-
-const showHide = (state: ReduxInnerType, uuid: string) => {
-  if (state.value[`${uuid}`].showChildrens === true) {
-    state.value = {
-      ...state.value,
-      [uuid]: {
-        ...state.value[uuid],
-        showChildrens: !state.value[uuid].showChildrens,
-      },
-    };
-    state.value[`${uuid}`].childrens.map((childrenUuid: string) =>
-      showHide(state, childrenUuid)
-    );
-  }
-};
-
-const showHideElement = (state: ReduxInnerType, uuid: string) => {
-  state.value = {
-    ...state.value,
-    [uuid]: {
-      ...state.value[uuid],
-      showChildrens: !state.value[uuid].showChildrens,
-    },
-  };
-  if (state.value[`${uuid}`].showChildrens === true) {
-    state.value[`${uuid}`].childrens.map((childrenUuid: string) =>
-      showHide(state, childrenUuid)
-    );
-  }
-};
+import deleteElement from "./deleteElement";
+import showHideElement from "./showHideElement";
 
 const dataSlice = createSlice({
   name: "data",
